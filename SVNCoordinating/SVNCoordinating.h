5a7a5c5f8070291d444ccd737bf23fd4671cbaa9
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-13 16:44:12 -0700

    Initial Commit

---
+++ SVNCoordinating/SVNCoordinating.h
@@ -0,0 +1,19 @@
+//
+//  SVNCoordinating.h
+//  SVNCoordinating
+//
+//  Created by Aaron Dean Bikis on 4/13/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNCoordinating.
+FOUNDATION_EXPORT double SVNCoordinatingVersionNumber;
+
+//! Project version string for SVNCoordinating.
+FOUNDATION_EXPORT const unsigned char SVNCoordinatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNCoordinating/PublicHeader.h>
+
+
